ab380b7c2a5c98162e16c2fe2f1722f7564e7009
AndreiShpinko <73303318+AndreiShpinko@users.noreply.github.com>
2020-10-23 15:06:24 +0300

    new site

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    $('.slider--team').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+    $('.slider--reviews').slick({
+        slidesToShow:4,
+        infinite: true,
+        rows: 2,
+        arrows: false,
+        variableWidth: true,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+    });
+    $('.slider--dev').slick({
+        slidesToShow: 1,
+        infinite: true,
+        variableWidth: true,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+    $('.menu__burger').click(function(event){
+        $('.menu__burger, .menu__list').toggleClass('active');
+        $('.body').toggleClass('lock');
+    });
+});
